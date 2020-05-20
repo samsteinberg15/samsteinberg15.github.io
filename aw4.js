c67eeecf87dfb89c5ad01563a799a19e7c6e4e92
Sam Steinberg <steinberg.s18@gmail.com>
2020-05-20 18:34:03 -0400

    com

--- aw4.js
+++ aw4.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    /*$('#hidepicture').on('click',function(){
+    $('#hidepicture').on('click',function(){
       $('#jeff').fadeOut(1000);
     })
     $('#showpicture').on('click',function(){
@@ -8,22 +8,34 @@ $(document).ready(function(){
     $('#changeColor').on('click',function(){
       $('#thingsIlike').css('background-color', 'red').css('color','blue');
       $('#firstheading').html(' Welcome to our Class');
-    })*/
-
+    })
     $.ajax({
       url: 'https://randomuser.me/api/?results=20',
       dataType: 'json',
       success: function(data) {
         console.log(data);
-        $.each(data.results, function(key,person){
+        $.each(data.results, function(key,person) {
           console.log(person.name.title + ' ' + person.name.first + ' ' + person.name.last);
-          $('#users').append('<div class= "col-md-2">' + '<img src= "'+ person.picture.large + '" class="img-fluid rounded-circle">' + ' '  + person.name.first + ' ' + person.name.last +  ' ' + person.nat  + ' ' + '</div>');
-
-
-
-
+          let flag = "flag-icon flag-icon-" + (person.nat).toLowerCase()
+          let individual = '<div class= "col-md-2">' +
+          '<img src= "'+ person.picture.large + '" class="img-fluid rounded-circle">' + ' '  +
+          "<div class='caption center-block'>" + person.name.first + ' ' + person.name.last + '</div>' + '<span class="'+ flag + '"></span>'
+          if (person.gender == "male") {
+            individual += "<section class=icons>" +
+            "<i class='male fa fa-github'></i>" +
+            "<i class='male fa fa-facebook'></i>" +
+            "<i class='male fa fa-twitter'></i>" +
+            "</section>"
+          }
+          else if (person.gender == "female") {
+            individual += "<section class=icons>" +
+            "<i class='female fa fa-github'></i>" +
+            "<i class='female fa fa-facebook'></i>" +
+            "<i class='female fa fa-twitter'></i>" +
+            "</section>"
+            }
+            $('#users').append(individual);
         })
-
   }
 });
 });
